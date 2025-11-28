5f2685888858b1f3cf29e88c0eb88709404d94d6
jhantu626 <jhantubala626@gmail.com>
2025-11-28 15:52:26 +0530

    feat: Add Device Sequelize model with business association.

---
+++ models/Devices.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require("sequelize");
+const { sequilize } = require("../config/db");
+const { Business } = require("./Business");
+
+const Device = sequilize.define("Device", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  fcmToken: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  deviceType: {
+    type: DataTypes.ENUM("android", "ios"),
+    allowNull: false,
+  },
+  deviceModel: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  deviceName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  deviceUniqueKey: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  businessId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Business",
+      key: "id",
+    },
+  },
+});
+
+Device.belongsTo(Business, {
+  foreignKey: "businessId",
+  as: "business",
+});
+
+module.exports = { Device };
