8b8ec8e6e92accd6500bc3633a331816f0ac3840
jhantu626 <jhantubala626@gmail.com>
2025-11-28 18:06:14 +0530

    feat: add API endpoint for deleting devices by unique key

--- routers/DeviceRoutes.js
+++ routers/DeviceRoutes.js
@@ -2,23 +2,25 @@ const express = require("express");
 const { Device } = require("../models/Devices");
 const router = express.Router();
 
-router.delete("remove-device", async (req, res) => {
+router.delete("/remove-device/:deviceUniqueKey", async (req, res) => {
   try {
-    const { deviceUniqueId } = req.body;
-    if (!deviceUniqueId)
+    const { deviceUniqueKey } = req.params;
+    console.log(deviceUniqueKey);
+    if (!deviceUniqueKey)
       return res.json({
         message: "Device unique id is required",
         status: false,
       });
     const device = await Device.findOne({
-      where: { deviceUniqueId },
-      attributes: ["id"],
+      where: { deviceUniqueKey },
     });
+    console.log("device", device);
     if (!device)
       return res.json({ message: "Device not found", status: false });
     await device.destroy();
     return res.json({ message: "Device removed successfully", status: true });
   } catch (error) {
+    console.log(error);
     return res.json({ message: "Something went wrong", status: false });
   }
 });
